7227bbe00b1972fa66f15d7db6c55b13b1a3d2a8
Ivan Clark Gomez <gomzclark@gmail.com>
2023-07-04 02:19:49 +0800

    Added complete console app solution for robotapp test.

---
+++ RobotApp/RobotApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using static RobotApp.Robot;
+
+namespace RobotApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Robot robot = null;
+            Console.WriteLine("WELCOME TO ROBOT SIMULATION APP! (Type QUIT to exit)");
+            Console.WriteLine("Start by using the PLACE command.");
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var command = line.Split(' ');
+
+                if (command[0] != "PLACE" && command[0] != "QUIT" && robot == null)
+                {
+                    command[0] = "INVALID";
+                }
+
+                switch (command[0])
+                {
+                    case "PLACE":
+                        try
+                        {
+                            //parameter validations:
+                            var parameters = command[1].Split(',');
+                            var positionX = int.Parse(parameters[0]);
+                            var positionY = int.Parse(parameters[1]);
+                            
+                            if (parameters.Length == 3)
+                            {
+                                // direction REQUIRED when NOT placed
+                                var direction = (Directions)Enum.Parse(typeof(Directions), parameters[2]);
+                                robot = new Robot();
+                                robot.Place(positionX, positionY, direction);
+                            }
+                            else if (parameters.Length == 2 && robot != null)
+                            {
+                                // direction OPTIONAL when already placed
+                                robot.Place(positionX, positionY);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Number of parameters is invalid!");
+                            }
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Invalid parameters!");
+                        }
+                        break;
+                    case "MOVE":
+                        robot.Move();
+                        break;
+                    case "LEFT":
+                        robot.Left();
+                        break;
+                    case "RIGHT":
+                        robot.Right();
+                        break;
+                    case "REPORT":
+                        robot.Report();
+                        break;
+                    case "QUIT":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+                }
+            }
+        }
+    }
+}
+
+//---------------------
+//|0,4|1,4|2,4|3,4|4,4|
+//--------------------- 
+//|0,3|1,3|2,3|3,3|4,3|
+//--------------------- 
+//|0,2|1,2|2,2|3,2|4,2|
+//--------------------- 
+//|0,1|1,1|2,1|3,1|4,1|
+//--------------------- 
+//|0,0|1,0|2,0|3,0|4,0|
+//--------------------- 
---
+++ RobotApp/RobotApp/Robot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace RobotApp
+{
+
+    class Robot
+    {
+        public enum Directions { NORTH, EAST, SOUTH, WEST }
+
+        // these will be the properties of the robot
+        private int? positionX;
+        private int? positionY;
+        private Directions? direction;
+
+        // these will be the actions of the robot
+        public void Place(int positionX, int positionY, Directions? direction = null)
+        {
+            // you can't place off the edge :)
+            if (positionX > 4 || positionX < 0 || positionY > 4 || positionY < 0)
+                throw new Exception();
+
+            this.positionX = positionX;
+            this.positionY = positionY;
+            this.direction = direction ?? this.direction;
+        }
+
+        public void Move()
+        {
+            // when facing north, increment y axis by 1
+            if (direction == Directions.NORTH && positionY + 1 <= 4)
+                positionY++;
+
+            // when facing south, decrement y axis by 1
+            else if (direction == Directions.SOUTH && positionY - 1 >= 0)
+                positionY--;
+
+            // when facing east, increment x axis by 1
+            else if (direction == Directions.EAST && positionX + 1 <= 4)
+                positionX++;
+
+            // when facing west, decrement x axis by 1
+            else if (direction == Directions.WEST && positionX - 1 >= 0)
+                positionX--;
+
+            else
+                Console.WriteLine("You will fall off the table! That action is prevented.");
+        }
+
+        public void Left()
+        {
+            // rotating counter clock-wise
+            direction = direction != Directions.NORTH ? direction - 1 : Directions.WEST;
+        }
+
+        public void Right()
+        {
+            // rotating clock-wise
+            direction = direction != Directions.WEST ? direction + 1 : Directions.NORTH;
+        }
+
+        public void Report()
+        {
+            Console.WriteLine($"X:{positionX}, Y:{positionY}, F:{direction}");
+        }
+    }
+}
